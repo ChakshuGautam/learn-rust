b2bb37c9a3413f09decb25e969a66dfce7d573ba
ChakshuGautam <chaks.gautam@gmail.com>
2023-09-10 13:28:44 +0000

    Feat: EC Assignment - Task 1

---
+++ empowered_coder/assignments/library.rs
@@ -0,0 +1,16 @@
+struct Person {
+    name: String,
+    age: usize,
+}
+
+struct Book {
+    title: String,
+    author: String,
+    is_available: bool
+}
+
+
+struct Library {
+    books: Vec<Book>
+}
+
